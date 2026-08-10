08b5473a5ff661240fa5ff5a6b52c0b460976e11
prio12 <maksudurprio.mp@gmail.com>
2026-08-10 21:36:54 +0600

    feat: added registration validation

--- backend/DTOs/Auth/RegisterRequest.cs
+++ backend/DTOs/Auth/RegisterRequest.cs
@@ -1,11 +1,19 @@
-using Learnova.Models;
-
 namespace Learnova.DTO.Auth;
 
+using Learnova.Models;
+using System.ComponentModel.DataAnnotations;
 public class RegisterRequest
 {
+    [Required]
+    [StringLength(24)]
     public required string Name { get; set; }
+
+    [Required]
+    [EmailAddress]
     public required string Email { get; set; }
+
+    [Required]
+    [MinLength(6)]
     public required string Password { get; set; }
 
 }
