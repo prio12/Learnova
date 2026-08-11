0d013be3cddefb21d09d9c51b717c5b782844953
prio12 <maksudurprio.mp@gmail.com>
2026-08-12 02:07:25 +0600

    feat: Assignment request validation added

--- backend/DTOs/Assignment/AssignmentRequest.cs
+++ backend/DTOs/Assignment/AssignmentRequest.cs
@@ -1,13 +1,44 @@
+
 namespace Learnova.DTO.Assignment;
+
+using System.ComponentModel.DataAnnotations;
 
 public class AssignmentRequest
 {
+    [Required]
+    [StringLength(100)]
     public required string Title { get; set; }
+
+    [Required]
+    [StringLength(1000)]
     public required string Description { get; set; }
 
+    [Required]
     public Guid CourseId { get; set; }
+
+    [Required]
     public Guid SubjectId { get; set; }
 
+    [Required]
+    [FutureDate]
     public DateTime Deadline { get; set; }
+
+    [Range(1, 1000)]
     public int MaximumMarks { get; set; }
+}
+
+public class FutureDateAttribute : ValidationAttribute
+{
+    public override bool IsValid(object? value)
+    {
+        if (value is not DateTime date)
+            return false;
+
+        return date > DateTime.UtcNow;
+    }
+
+    public override string FormatErrorMessage(string name)
+    {
+        return $"{name} must be a future date.";
+    }
 }
